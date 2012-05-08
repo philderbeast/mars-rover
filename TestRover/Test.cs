9782d39929b92cd6d45830de9a5c1ea13ceef49d
philderbeast <phil.dejoux@blockscope.com>
2012-05-08 16:06:05 +1000

    Workaround git line endings.

--- TestRover/Test.cs
+++ TestRover/Test.cs
@@ -9,8 +9,8 @@ namespace TestRover
         public void Do_the_two_rovers_in_the_problem_statement_move_as_expected()
         {
             var input = Resources.TestInput;
-            var output = Trim(MarsRover.Program.Run(input));
-            var testOutput = Trim(Resources.TestOutput);
+            var output = Normalize(MarsRover.Program.Run(input));
+            var testOutput = Normalize(Resources.TestOutput);
             Assert.AreEqual(output, testOutput);
         }
 
@@ -20,6 +20,16 @@ namespace TestRover
         private static string Trim(string input)
         {
             return input.Trim('\r', '\n', ' ');
+        }
+
+        /// <summary>
+        /// Files added to git can have their line endings altered.
+        /// Standardize on one line ending for the tests, working around git.
+        /// <see cref="http://help.github.com/line-endings/"/>
+        /// </summary>
+        private static string Normalize(string s)
+        {
+            return Trim(s.Replace("\r\n", "\n"));
         }
     }
 }
